97838e0ed3ad185a4836e4b3ca5d5d48056287be
NaveenJsr <naveenjsr99@gmail.com>
2023-03-22 16:53:36 +0530

    Exercise 1.10: unicafe step5 Complete

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,66 +1,45 @@
-import { useState } from 'react'
-
-const Statistics = ( { good, neutral, bad } ) =>
-{
-  const total = good + neutral + bad
-  const average = ( good - bad ) / total || 0 // avoid division by zero
-  const positivePercentage = ( good / total ) * 100 || 0 // avoid NaN
-
-  if ( total === 0 )
-  {
-    return (
-      <div>
-        <h2>Statistics</h2>
-        <p>No feedback given</p>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h2>Statistics</h2>
-      <p>good { good }</p>
-      <p>neutral { neutral }</p>
-      <p>bad { bad }</p>
-      <p>all { total }</p>
-      <p>average { average }</p>
-      <p>positive { positivePercentage }%</p>
-    </div>
-  )
-}
-
+import { useState } from 'react';
+import Button from './Button';
+import StatisticLine from './StatisticLine';
 
 const App = () =>
 {
-  // save clicks of each button to its own state
-  const [ good, setGood ] = useState( 0 )
-  const [ neutral, setNeutral ] = useState( 0 )
-  const [ bad, setBad ] = useState( 0 )
+  const [ good, setGood ] = useState( 0 );
+  const [ neutral, setNeutral ] = useState( 0 );
+  const [ bad, setBad ] = useState( 0 );
 
-  const handleGoodClick = () =>
-  {
-    setGood( good + 1 )
-  }
+  const handleGoodClick = () => setGood( good + 1 );
+  const handleNeutralClick = () => setNeutral( neutral + 1 );
+  const handleBadClick = () => setBad( bad + 1 );
 
-  const handleNeutralClick = () =>
-  {
-    setNeutral( neutral + 1 )
-  }
-
-  const handleBadClick = () =>
-  {
-    setBad( bad + 1 )
-  }
+  const total = good + neutral + bad;
+  const average = ( good - bad ) / total || 0; // avoid division by zero
+  const positivePercentage = ( good / total ) * 100 || 0; // avoid NaN
 
   return (
     <div>
-      <h1>Give feedback</h1>
-      <button onClick={ handleGoodClick }>good</button>
-      <button onClick={ handleNeutralClick }>neutral</button>
-      <button onClick={ handleBadClick }>bad</button>
-      <Statistics good={ good } neutral={ neutral } bad={ bad } />
+      <h1>give feedback</h1>
+      <Button onClick={ handleGoodClick } text="good" />
+      <Button onClick={ handleNeutralClick } text="neutral" />
+      <Button onClick={ handleBadClick } text="bad" />
+      <h2>statistics</h2>
+      { total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <div>
+          <StatisticLine text="good" value={ good } />
+          <StatisticLine text="neutral" value={ neutral } />
+          <StatisticLine text="bad" value={ bad } />
+          <StatisticLine text="all" value={ total } />
+          <StatisticLine text="average" value={ average } />
+          <StatisticLine
+            text="positive"
+            value={ `${ positivePercentage }%` }
+          />
+        </div>
+      ) }
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ unicafe/src/StatisticLine.js
@@ -0,0 +1,7 @@
+const StatisticLine = ( { text, value } ) => (
+    <p>
+        { text } { value }
+    </p>
+);
+
+export default StatisticLine;
