20bb28e6f1696a4b9c6cff73c220cfeca6ebfe74
NaveenJsr <naveenjsr99@gmail.com>
2023-03-22 16:11:46 +0530

    Exercise 1.6: unicafe step1 Complete

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () =>
+{
+  // save clicks of each button to its own state
+  const [ good, setGood ] = useState( 0 )
+  const [ neutral, setNeutral ] = useState( 0 )
+  const [ bad, setBad ] = useState( 0 )
+
+  const handleGoodClick = () =>
+  {
+    setGood( good + 1 )
+  }
+
+  const handleNeutralClick = () =>
+  {
+    setNeutral( neutral + 1 )
+  }
+
+  const handleBadClick = () =>
+  {
+    setBad( bad + 1 )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={ handleGoodClick }>good</button>
+      <button onClick={ handleNeutralClick }>neutral</button>
+      <button onClick={ handleBadClick }>bad</button>
+      <h2>Statistics</h2>
+      <p>good { good }</p>
+      <p>neutral { neutral }</p>
+      <p>bad { bad }</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
